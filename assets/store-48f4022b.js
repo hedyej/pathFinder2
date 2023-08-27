1888881f74cdcafe7e2dfc8ffa938ef9f32fae12
hedyej <hedyej@users.noreply.github.com>
2023-08-27 16:31:29 +0000

    deploy: 14ad4cd1e21d1d83ce6d1c20664d051c9ff0432b

---
+++ assets/store-48f4022b.js
@@ -0,0 +1,1 @@
+import{B as e}from"./index-207066be.js";const o=t=>e("get","stores",t),r=t=>e("get","comments",t),g=t=>e("get",`stores/${t}`);export{o as a,g as b,r as g};
--- assets/store-4a7da84b.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./index-596e7e29.js";const o=t=>e("get","stores",t),r=t=>e("get","comments",t),g=t=>e("get",`stores/${t}`);export{o as a,g as b,r as g};
